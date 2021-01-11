1a697378a3368c26b72591266b643e7fd780daa6
jValdez <jdv_rodriguez@hotmail.com>
2021-01-10 19:04:25 -0600

    componentes de js y vue cargados a webpack

---
+++ src/app/index.js
@@ -0,0 +1,1 @@
+alert('Vue js va aqui');
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const { VueLoaderPlugin } = require('vue-loader');
+
+module.exports = {
+    entry: './src/app/index.js',
+    output: {
+        path: __dirname + '/src/public',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+                
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+};
