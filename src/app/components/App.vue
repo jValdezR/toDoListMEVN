c10c3efc7a022b180e167449a5e1a6fe6b32b0e6
jValdez <jdv_rodriguez@hotmail.com>
2021-01-11 21:07:15 -0600

    get y post en front

--- src/app/components/App.vue
+++ src/app/components/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <nav class="navbar navbar-light bg-light">
-        <a href="/" class="navbar-brand">Proyecto</a>
+            <a href="/" class="navbar-brand">Proyecto</a>
         </nav>
 
         <div class="container">
@@ -11,17 +11,37 @@
                         <div class="card-body">
                             <form @submit.prevent="addTask">
                                 <div class="form-group">
-                                     <input type="text" placeholder="Agrega una tarea" class="form-control">
+                                     <input type="text" v-model="task.title"
+                                     placeholder="Agrega una tarea" 
+                                     class="form-control">
                                 </div>
-                            
                                 <div class="form-group">
-                                    <textarea class="form-control" placeholder="Agrega una descripcion" cols="30" rows="10"></textarea>
+                                    <textarea class="form-control" v-model="task.descripcion"
+                                    placeholder="Agrega una descripcion" 
+                                    cols="30" rows="10"></textarea>
                                 </div>            
-                           
                                 <button class="btn btn-primary btn-block">Guardar</button>
                             </form>
                         </div>
                     </div>
+                </div>
+                <div class="col-md-7">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Tarea</th>
+                                <th>Descripcion</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="task of tasks">
+                                <td>
+                                    {{task.title}}
+                                </td>
+                                <td>{{task.descripcion}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
@@ -29,19 +49,48 @@
 </template>
 
 <script>
+
+
+
 export default {
     data() {
         return {
             task: {
                 title: '',
                 descripcion: ''
-            }
+            },
+            tasks: []
         }
     },
+    created() {
+        this.getTask();
+    },
     methods: {
+        getTask(){
+            fetch('/tasks')
+            .then(res => res.json())
+            .then( data  => {
+                console.log(data);
+                this.tasks = data;
+            })
+        },
         addTask() {
-            console.log(this.task);
+            fetch('/tasks',{
+                method: 'POST',
+                body: JSON.stringify(this.task),
+                headers: {
+                   'Accept': 'application/json',
+                   'Content-type': 'application/json'
+                   }
+                })
+                .then( res => res.json())
+                .then( data => console.log(data))
+
+                this.task.title = '';
+                this.task.descripcion = '';
+            },
+
+            
         }
     }
-}
 </script>
